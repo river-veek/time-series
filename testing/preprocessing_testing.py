bed0ad09453f9f27cd1d4b8b7ce33ca314bd5475
cjordal <cjordal@uoregon.edu>
2021-01-29 19:11:48 -0800

    added test case

--- testing/preprocessing_testing.py
+++ testing/preprocessing_testing.py
@@ -11,12 +11,12 @@ To run nosetests (from 'time-series/' directory):
 import numpy as np
 import sys
 sys.path.append("../")
-import time_series as TS
 import file_io as fio
 from preprocessing import *
+import nose
 
 
-
+'''
 def test1_timeSeriesData1():
 	"""
 	test time series creation from timeSeriesData1 files
@@ -34,9 +34,25 @@ def test1_timeSeriesData1():
 
 	ts = fio.read_from_file(fname)
 	#logarithm(ts)
-
+'''
 	
 
+def test_general_scaling():
+	"""
+	Test general use case of scaling function
+	"""
+	test_input = {
+		"Times": [0, 1, 2, 3, 4, 5],
+		"Values": [1, 0, 9, 10, 8, 4]
+	}
+	df_test_input = pd.DataFrame(test_input)
+	test_output = {
+		"Times": [0, 1, 2, 3, 4, 5],
+		"Values": [0.1, 0, .9, 1, .8, .4]
+	}
+	df_test_output = pd.DataFrame(test_output)
+	df_actual_output = scaling(df_test_input)
+	assert list(df_actual_output.iloc[:, -1]) == list(df_test_output.iloc[:, -1])
 
 
 
@@ -47,8 +63,7 @@ def test1_timeSeriesData1():
 
 
 
-
-
+'''
 def main():
 	test1_timeSeriesData1()
 	#test2_timeSeriesData2()
@@ -59,3 +74,4 @@ def main():
 
 
 main()
+'''
