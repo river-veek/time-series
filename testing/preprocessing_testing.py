55eb61f6de205c4fec606ce3bccf921e33bddacc
cjordal <cjordal@uoregon.edu>
2021-02-03 17:57:01 -0800

    removed git merge stuff

--- testing/preprocessing_testing.py
+++ testing/preprocessing_testing.py
@@ -160,7 +160,6 @@ def test_no_NaN_longest_continuous_run():
 
     assert longest_continuous_run(df1).equals(df2)
 
-<<<<<<< HEAD
 def test_all_NaN_longest_continuous_run():
     df1 = pd.DataFrame({'Time': [0, 1, 2, 3, 4],
                         'Daily Top': ["GME", "AMC", "BB", "DOGE", "NOK"],
@@ -171,7 +170,6 @@ def test_all_NaN_longest_continuous_run():
 
     assert longest_continuous_run(df1).equals(df2)
 
-=======
 ###################
 # DIFFERENCE()
 ###################
@@ -209,7 +207,6 @@ def test_negatives_difference():
 	df_test_output = pd.DataFrame(test_output)
 	df_actual_output = difference(df_test_input)
 	assert list(df_actual_output.iloc[:, -1]) == list(df_test_output.iloc[:, -1])
->>>>>>> 8aeddf443e4d3ca0a75788570a0c5833cd7671da
 
 def test_empty_difference():
 	"""
